a7b888b1191811c75a74a5bd337f0993012b27b8
thiagopaiva99 <thiagopaiva99@gmail.com>
2020-07-19 23:30:15 -0300

    Working with more exemples of pure functions

--- functional/pure-functions.ts
+++ functional/pure-functions.ts
@@ -15,5 +15,28 @@ function areaCircPure(radius: number, piValue: number): number {
     return radius * radius * piValue;
 }
 
-console.log(areaCircImpure(2)); // 12.56
-console.log(areaCircPure(2, 5)); // 20
+// Pure or Impure? Impure, this function still have an external dependecy
+// Also, we can not determine what is the output value since it is a random number
+function generateRandomNumber(min: number, max: number): number {
+    const factor = max - min + 1;
+    return (Math.random() * factor) + min;
+}
+
+// Pure or Impure? Pure!
+function sumPure(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+let executionsAmount = 0;
+
+// Pure or Impure? Impure, because it have side effects out ot function scope
+function sumImpure(num1: number, num2: number): number {
+    executionsAmount++;
+    return num1 + num2;
+}
+
+console.log(areaCircImpure(2));
+console.log(areaCircPure(2, 5));
+console.log(generateRandomNumber(2, 4));
+console.log(sumPure(5, 9));
+console.log(sumImpure(5, 9));
